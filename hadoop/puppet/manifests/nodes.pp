bdfefe039dd3b64a66f5d303b7dc13fa2c1f2887
Fabian Steeg <fsteeg@gmail.com>
2012-09-14 16:48:00 +0200

    Initial Puppet-based Hadoop cluster configuration and documentation

---
+++ hadoop/puppet/manifests/nodes.pp
@@ -0,0 +1,20 @@
+node hydra1 {
+    include java
+    include hadoop
+}
+
+node hydra2 {
+    include java
+    include hadoop
+}
+
+node hydra3 {
+    include java
+    include hadoop
+}
+
+node hydra4 {
+    include java
+    include hadoop
+}
+
