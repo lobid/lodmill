bc192d58dcdb11846f91e1a94a0dcd16adf0d506
Fabian Steeg <fsteeg@gmail.com>
2012-11-12 16:17:24 +0100

    Set up ngram index config in preparation for auto-suggest feature

--- lodmill-ld/src/test/java/org/lobid/lodmill/hadoop/IndexFromHdfsInElasticSearchTests.java
+++ lodmill-ld/src/test/java/org/lobid/lodmill/hadoop/IndexFromHdfsInElasticSearchTests.java
@@ -8,10 +8,12 @@ import java.net.URI;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.elasticsearch.client.Client;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.json.JSONException;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -36,6 +38,7 @@ public class IndexFromHdfsInElasticSearchTests {
 	private static final String HDFS_SERVER = "hdfs://localhost:9000/";
 	private static FileSystem hdfs = null;
 	private static IndexFromHdfsInElasticSearch indexer = null;
+	private static TransportClient client;
 
 	@BeforeClass
 	public static void upload() throws IOException {
@@ -43,7 +46,7 @@ public class IndexFromHdfsInElasticSearchTests {
 		final Path srcPath = new Path(TEST_FILE);
 		hdfs.copyFromLocalFile(srcPath, new Path(DATA_1));
 		hdfs.copyFromLocalFile(srcPath, new Path(DATA_2));
-		final Client client =
+		client =
 				new TransportClient(ImmutableSettings.settingsBuilder()
 						.put("cluster.name", ES_CLUSTER_NAME).build())
 						.addTransportAddress(ES_SERVER);
@@ -60,6 +63,31 @@ public class IndexFromHdfsInElasticSearchTests {
 	public void testIndexAll() throws IOException, JSONException,
 			InterruptedException {
 		Assert.assertEquals(50, indexer.indexAll("json-es-test/").size());
+	}
+
+	@Test
+	public void testNGram() throws IOException, JSONException,
+			InterruptedException {
+		indexer.indexAll("json-es-test/");
+		Thread.sleep(200); // it seems the ngram analyzer needs a moment
+		final SearchResponse response =
+				search("json-ld-index",
+						"http://purl.org/dc/elements/1.1/creator#preferredNameForThePerson",
+						"loft");
+		Assert.assertTrue(
+				"Substring of actual index term should yield results due to ngram config",
+				response.getHits().iterator().hasNext());
+	}
+
+	private SearchResponse search(final String index, final String field,
+			final String term) {
+		final SearchResponse response =
+				client.prepareSearch(index)
+						.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+						.setQuery(QueryBuilders.termQuery(field, term))
+						.setFrom(0).setSize(10).setExplain(true).execute()
+						.actionGet();
+		return response;
 	}
 
 	@AfterClass
