289948852a3b6a97dd7165e56ac5a965e02c4eb9
Fabian Steeg <fsteeg@gmail.com>
2013-01-17 15:57:56 +0100

    Move hbz MARC ingest experiment into lodmill-rd and enable PMD

---
+++ lodmill-rd/src/main/java/org/lobid/lodmill/HbzMarcIngest.java
@@ -0,0 +1,126 @@
+/* Copyright 2013 Fabian Steeg. Licensed under the Eclipse Public License 1.0 */
+
+package org.lobid.lodmill;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import junit.framework.Assert;
+
+import org.culturegraph.metamorph.core.Metamorph;
+import org.culturegraph.metamorph.core.MetamorphErrorHandler;
+import org.culturegraph.metamorph.reader.MarcReader;
+import org.culturegraph.metamorph.reader.Reader;
+import org.culturegraph.metastream.sink.StringListMap;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Ingest the hbz Marc21 export and log errors to debug issues with the format.
+ * 
+ * @author Fabian Steeg (fsteeg)
+ */
+public final class HbzMarcIngest {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(HbzMarcIngest.class);
+	private static final String HBZ_MARC = "../../hbz.cg.20120725.mrc";
+	private static final String REPORT_RAW = HBZ_MARC + "-report-raw.txt";
+	private static final String REPORT_PROCESSED = HBZ_MARC
+			+ "-report-processed.txt";
+	private final Reader reader = new MarcReader();
+	private final Metamorph metamorph = new Metamorph(Thread.currentThread()
+			.getContextClassLoader()
+			/* actual config file is in culturegraph-cluster */
+			.getResourceAsStream("mapping/ingest.marc21.xml"));
+
+	private final SortedSet<String> errorSet = new TreeSet<String>();
+	private final Map<String, Integer> errorMap =
+			new HashMap<String, Integer>();
+
+	@Test
+	public void ingest() throws IOException {
+		final StringListMap map = new StringListMap();
+		reader.setReceiver(metamorph).setReceiver(map);
+		final BufferedWriter rawReportWriter =
+				new BufferedWriter(new FileWriter(REPORT_RAW));
+		metamorph.setErrorHandler(new MetamorphErrorHandler() {
+			@Override
+			public void error(final Exception exception) {
+				final String name = exception.getClass().getSimpleName();
+				final String errorMessage =
+						String.format("Metamorph error (%s): %s", name,
+								exception.getMessage());
+				processError(rawReportWriter, name, errorMessage);
+			}
+		});
+		final BufferedReader scanner =
+				new BufferedReader(new FileReader(HBZ_MARC));
+		int all = 0;
+		String line = null;
+		while ((line = scanner.readLine()) != null) { // NOPMD (idiomatic usage)
+			all++;
+			try {
+				reader.read(line);
+			} catch (Exception e) {
+				final String name = e.getClass().getSimpleName();
+				final String errorMessage =
+						String.format("Metastream error (%s): %s, record: %s",
+								name, e.getMessage(), line);
+				processError(rawReportWriter, name, errorMessage);
+			}
+		}
+		rawReportWriter.close();
+		Assert.assertTrue("Raw report file should exist",
+				new File(REPORT_RAW).exists());
+		writeProcessedReport(scanner, all);
+		Assert.assertTrue("Processed report file should exist", new File(
+				REPORT_PROCESSED).exists());
+	}
+
+	private void writeProcessedReport(final BufferedReader scanner,
+			final int all) throws IOException {
+		final BufferedWriter processedReportWriter =
+				new BufferedWriter(new FileWriter(REPORT_PROCESSED));
+		int err = 0;
+		for (Integer i : errorMap.values()) {
+			err += i;
+		}
+		final String summary =
+				String.format("Processed %s records, got %s errors:", all, err);
+		System.out.println(summary);
+		processedReportWriter.write(summary + "\n");
+		for (String s : errorMap.keySet()) {
+			System.out.println(String.format("%s: %s", s, errorMap.get(s)));
+			processedReportWriter.write(String.format("%s: %s\n", s,
+					errorMap.get(s)));
+		}
+		scanner.close();
+		for (String string : errorSet) {
+			processedReportWriter.write(string + "\n");
+		}
+		processedReportWriter.close();
+	}
+
+	private void processError(final BufferedWriter fullReportWriter,
+			final String name, final String errorMEssage) {
+		LOG.error(errorMEssage);
+		errorSet.add(errorMEssage);
+		errorMap.put(name,
+				(errorMap.containsKey(name) ? errorMap.get(name) : 0) + 1);
+		try {
+			fullReportWriter.write(errorMEssage + "\n");
+		} catch (IOException e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+}
