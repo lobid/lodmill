1e9d4f24c6780e08bef750e315a6a797690b09ff
Fabian Steeg <fsteeg@gmail.com>
2013-02-21 11:57:15 +0100

    Update release script for new p2 repo location

--- metatext/release.sh
+++ metatext/release.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 mvn clean install
-P2=../../lobid.github.com/p2
-rm -rf $P2
-cp -R org.lobid.metatext.p2/target/repository $P2
-cd $P2/..
-sh create-directory-listings.sh
-git add -A .
-# git commit
-# git push origin master
+scp -r org.lobid.metatext.p2/target/repository/* lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools/p2
