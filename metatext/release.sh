cc61a2aac530166cee0885cba082192d63c88c10
Fabian Steeg <fsteeg@gmail.com>
2013-02-25 10:51:59 +0100

    Add upload of product zips to release script

--- metatext/release.sh
+++ metatext/release.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mvn clean install
 scp -r org.lobid.metatext.p2/target/repository/* lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools/p2
+scp org.lobid.metatext.p2/target/products/*.zip lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools
