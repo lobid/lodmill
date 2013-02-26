6dcdba0de6cc04ea47ec258ec27045993970899a
Fabian Steeg <fsteeg@gmail.com>
2013-02-26 11:48:22 +0100

    Generate lodmill-rd jar-with-dependencies as part of the build
    - Generate jar with mavn assembly:assembly
    - Copy correct properties files into jar
    - Copy jar into the org.lobid.metatext project
    - Add generated jar to .gitignore
    - Integrate jar deployment into Travis build
    - Only release in release.sh, don't build

--- metatext/release.sh
+++ metatext/release.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-mvn clean install
 scp -r org.lobid.metatext.p2/target/repository/* lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools/p2
 scp org.lobid.metatext.p2/target/products/*.zip lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools
