07435e233cb678afbb51ac5d6dd741b8a075b729
Fabian Steeg <fsteeg@gmail.com>
2013-03-04 15:59:09 +0100

    Update lodmill-ld scripts to work with local directory structure

--- lodmill-ld/doc/scripts/convert-orgs.sh
+++ lodmill-ld/doc/scripts/convert-orgs.sh
@@ -7,7 +7,7 @@ then
 fi
 
 export HADOOP=/opt/hadoop/hadoop
-export HADOOP_CLASSPATH=../target/lodmill-0.1.0-SNAPSHOT-jar-with-dependencies.jar
+export HADOOP_CLASSPATH=../../target/lodmill-ld-0.1.0-SNAPSHOT-jar-with-dependencies.jar
 
 $HADOOP/bin/hadoop fs -rmr $2
 $HADOOP/bin/hadoop org.lobid.lodmill.hadoop.NTriplesToJsonLd $1 $2 lobid-orgs-index json-ld-lobid-orgs
