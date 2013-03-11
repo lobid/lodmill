86ac2cf2a3b99ba609ecee38d8abbab19b139e72
Fabian Steeg <fsteeg@gmail.com>
2013-03-11 14:42:55 +0100

    Remove metatext and sample transformation (moved to separate repo)
    Moved to https://github.com/culturegraph/metafacture-ide

--- metatext/release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-scp -r org.lobid.metatext.p2/target/repository/* lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools/p2
-scp org.lobid.metatext.p2/target/products/*.zip lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools
