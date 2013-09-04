f230f7fca5d1c114db1b317154ef94d819277d48
Fabian Steeg <fsteeg@gmail.com>
2013-09-04 17:13:08 +0200

    Add fundertype and stocksize to HTML/RDFa view, fix contact QR
    Closes #126, closes #127, closes #128

--- lodmill-ui/test/tests/ScalaTests.scala
+++ lodmill-ui/test/tests/ScalaTests.scala
@@ -9,19 +9,24 @@ import play.api.libs.json.Json
 
 class ScalaTests extends AssertionsForJUnit {
 
-  var json: JsValue = Json.toJson(Map(
-    "@graph" -> List(
-      Map(
-        "postal-code" -> "4056",
-        "locality" -> "Basle"),
-      Map(
-        "name" -> "Uni Basel",
-        "identifier" -> "SzBaU"))))
+  var json: JsValue = Json.toJson(
+    Map[String, JsValue](
+      "@graph" -> Json.toJson(List[Map[String, JsValue]](
+        Map(
+          "postal-code" -> Json.toJson("4056"),
+          "locality" -> Json.toJson("Basle")),
+        Map(
+          "name" -> Json.toJson("Uni Basel"),
+          "identifier" -> Json.toJson("SzBaU"),
+          "http://purl.org/lobid/lv#contactqr" -> Json.toJson(
+            Map("@id" -> "http://lobid.org/media/DE-605_contactqr.png")))))))
 
   @Test def accessNested() {
     assert((json \\ "postal-code").head.asOpt[String].get === "4056")
     assert((json \\ "locality").head.asOpt[String].get === "Basle")
     assert((json \\ "name").head.asOpt[String].get === "Uni Basel")
     assert((json \\ "identifier").head.asOpt[String].get === "SzBaU")
+    assert(((json \\ "http://purl.org/lobid/lv#contactqr").head \ "@id").
+      asOpt[String].get === "http://lobid.org/media/DE-605_contactqr.png")
   }
 }
