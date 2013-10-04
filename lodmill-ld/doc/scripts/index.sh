21a8c7b9b61edaf150019191c1b5a8b79e5cc9ed
Fabian Steeg <fsteeg@gmail.com>
2013-10-04 14:04:33 +0200

    Reconcile job and indexing scripts (see #171)

--- lodmill-ld/doc/scripts/index.sh
+++ lodmill-ld/doc/scripts/index.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -ne 3 ]
 then
-  echo "Usage: `basename $0` INPUT"
+  echo "Usage: `basename $0` INPUT ES_SERVER ES_CLUSTER"
   exit 65
 fi
-export HADOOP=/opt/hadoop/hadoop
-#export HADOOP_CLIENT_OPTS="-XX:+UseConcMarkSweepGC -Xmx1024m $HADOOP_CLIENT_OPTS"
-$HADOOP/bin/hadoop jar ../../target/lodmill-ld-0.1.0-SNAPSHOT-jar-with-dependencies.jar org.lobid.lodmill.hadoop.IndexFromHdfsInElasticSearch hdfs://127.0.0.1:8020/ $1 127.0.0.1 es-lod-local
 
+export HADOOP=/opt/hadoop/hadoop
+
+INPUT=$1
+ES_SERVER=$2
+ES_CLUSTER=$3
+
+$HADOOP/bin/hadoop jar ../../target/lodmill-ld-0.2.0-SNAPSHOT-jar-with-dependencies.jar org.lobid.lodmill.hadoop.IndexFromHdfsInElasticSearch hdfs://10.9.0.10:8020/ $INPUT $ES_SERVER $ES_CLUSTER
