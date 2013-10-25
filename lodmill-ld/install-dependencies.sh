f1848b59704efb9d11aeb341fded4f21707648d9
Fabian Steeg <fsteeg@gmail.com>
2013-10-25 14:08:33 +0200

    Replace checkout scripts with snapshot repository setup (see #229)

--- lodmill-ld/install-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ../..
-git clone https://github.com/jsonld-java/jsonld-java.git jsonld-java-dependency
-cd jsonld-java-dependency
-git checkout 104d5b92cbbbfbb7d0e5d450ae669cb71f14adba
-mvn clean install -DskipTests=true
