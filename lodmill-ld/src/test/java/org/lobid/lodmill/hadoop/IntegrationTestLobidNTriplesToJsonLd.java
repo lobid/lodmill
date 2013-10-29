00a53a6acd82c3c98568f7ebbab8508b4dabf0eb
Fabian Steeg <fsteeg@gmail.com>
2013-10-29 11:00:09 +0100

    Resolve http://purl.org/dc/elements/1.1/contributor (see #224)

--- lodmill-ld/src/test/java/org/lobid/lodmill/hadoop/IntegrationTestLobidNTriplesToJsonLd.java
+++ lodmill-ld/src/test/java/org/lobid/lodmill/hadoop/IntegrationTestLobidNTriplesToJsonLd.java
@@ -88,6 +88,8 @@ public class IntegrationTestLobidNTriplesToJsonLd extends
 		assertTrue("Expect resolved stock size", result.contains("10,001 - 30,000"));
 		assertTrue("Expect resolved type triple for location",
 				result.contains("wgs84_pos#SpatialThing"));
+		assertTrue("Expect resolved contributor name",
+				result.contains("Zayer, Eric"));
 		assertFalse("Unresolved blank node should be filtered",
 				result.contains("preferredNameEntityForThePerson"));
 	}
