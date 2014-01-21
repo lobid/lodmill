ceb097df91b2391c86a5b1b3cb0a23a73b171bbc
Fabian Steeg <fsteeg@gmail.com>
2014-01-21 17:51:36 +0100

    Fix type information in views: use type of primary topic (see #257)

--- lodmill-ui/app/views/tags/TagHelper.scala
+++ lodmill-ui/app/views/tags/TagHelper.scala
@@ -1,3 +1,4 @@
+/* Copyright 2013, 2014 Fabian Steeg, hbz. Licensed under the Eclipse Public License 1.0 */
 package views.tags
 
 import play.api.libs.json.JsValue
@@ -14,5 +15,14 @@ object TagHelper {
       if ((labelObject \ "@language").asOpt[String].getOrElse("") == language)
     ) yield (labelObject \ "@value").asOpt[String].getOrElse("")
     Some(res.mkString(","))
+  }
+  def getPrimaryTopicType(node: JsValue): Option[JsValue] = {
+    val primaryTopic = (node \ "http://xmlns.com/foaf/0.1/primaryTopic")
+      .as[Map[String, String]].get("@id")
+    val res = for (
+      graphObject <- (node \ "@graph").as[List[JsValue]];
+      if ((graphObject \ "@id").asOpt[String].getOrElse("") == primaryTopic.getOrElse(""))
+    ) yield graphObject \ "@type"
+    Some(res(0))
   }
 }
