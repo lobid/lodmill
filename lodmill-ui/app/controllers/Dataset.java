c3407a2bdd4113e5b7f6dcd9ef464bd28ef89618
pascal Christoph <christoph@hbz-nrw.de>
2014-04-30 16:03:41 +0200

    Add serve JSON-LD when requesting a dataset
    * add dataset routes NWbib and RPB

--- lodmill-ui/app/controllers/Dataset.java
+++ lodmill-ui/app/controllers/Dataset.java
@@ -2,32 +2,108 @@
 
 package controllers;
 
+import models.Search;
+
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import play.Logger;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 /**
- * Path controller. Implements path-style routes and `about` redirects.
+ * Dataset controller. Implements path-style routes and `about` redirects.
  * 
- * @author pascal
+ * @author Pascal Christoph (dr0i)
  */
 public final class Dataset extends Controller {
+	// TODO don't hardwire index name to switch easily between staging and
+	// production
+	static final String DATA_INDEX = "lobid-provenance";
+	static final String DATA_TYPE = "json-ld-lobid-provenance";
 
 	private Dataset() {
 		/* No instantiation */
 	}
 
 	/**
-	 * Redirect to {@link #resourceAbout(String, String)}
+	 * Redirects to {@link #resourceAboutRPB(String, String)}
 	 */
 	@SuppressWarnings("javadoc")
-	public static Result resource(final String id, final String format) {
-		return redirect(routes.Dataset.resourceAbout(format));
+	public static Result resourceRPB(final String format) {
+		return redirect(routes.Dataset.resourceAboutRPB(format));
 	}
 
-	/***/
+	/**
+	 * Returns {@link #resourceAbout(String, String)}
+	 */
 	@SuppressWarnings("javadoc")
-	public static Result resourceAbout(final String id, final String format) {
-		return ok("ID: " + id); // TODO get dataset for ID from ES
+	public static Result resourceAboutRPB(final String id, final String format) {
+		return resourceAbout(id, format);
 	}
 
+	/**
+	 * Redirects to {@link #resourceAboutNWBib(String, String)}
+	 */
+	@SuppressWarnings("javadoc")
+	public static Result resourceNWBib(final String format) {
+		return redirect(routes.Dataset.resourceAboutNWBib(format));
+	}
+
+	/**
+	 * Returns {@link #resourceAbout(String, String)}
+	 */
+	@SuppressWarnings("javadoc")
+	public static Result resourceAboutNWBib(final String id, final String format) {
+		return resourceAbout(id, format);
+	}
+
+	/**
+	 * Redirects to {@link #resourceAboutEdoweb(String, String)}
+	 */
+	@SuppressWarnings("javadoc")
+	public static Result resourceEdoweb(final String format) {
+		return redirect(routes.Dataset.resourceAboutEdoweb(format));
+	}
+
+	/**
+	 * Returns {@link #resourceAbout(String, String)}
+	 */
+	@SuppressWarnings("javadoc")
+	public static Result resourceAboutEdoweb(final String id, final String format) {
+		return resourceAbout(id, format);
+	}
+
+	// TODO : format, content negotiation
+	@SuppressWarnings("javadoc")
+	private static Result resourceAbout(final String id, final String format) {
+		try {
+			MatchQueryBuilder query = QueryBuilders.matchQuery("_id", id);
+			SearchResponse response = search(DATA_INDEX, query, DATA_TYPE);
+			boolean found = response.getHits().getTotalHits() > 0;
+			return !found ? notFound() : ok(Json.parse("["
+					+ response.getHits().getAt(0).getSourceAsString() + "]"));
+		} catch (Exception x) {
+			x.printStackTrace();
+			return internalServerError(x.getMessage());
+		}
+	}
+
+	private static SearchResponse search(String index, QueryBuilder queryBuilder,
+			String type) {
+		SearchRequestBuilder requestBuilder =
+				Search.client.prepareSearch(index)
+						.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+						.setQuery(queryBuilder).setTypes(type);
+		Logger.debug("Request:\n" + requestBuilder);
+		SearchResponse response =
+				requestBuilder.setExplain(false).execute().actionGet();
+		Logger.debug("Response:\n" + response);
+		return response;
+	}
 }
