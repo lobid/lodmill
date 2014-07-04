79676bdcffb17bf04d2a4253bdeb87ddac305243
pascal Christoph <christoph@hbz-nrw.de>
2014-07-04 18:03:41 +0200

    Add apache URL encoder
    * see #504 :
    
    * update guava to have access to UrlEscapers
    * add function in morph to encode item
    * add morph function description
    * add schemata used to include morph function
    * update test file

---
+++ lodmill-rd/src/main/java/org/lobid/lodmill/URLEncode.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2014 hbz, Pascal Christoph
+ *
+ *  Licensed under the Apache License, Version 2.0 the "License";
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.lobid.lodmill;
+
+import org.culturegraph.mf.morph.functions.AbstractSimpleStatelessFunction;
+
+import com.google.common.net.UrlEscapers;
+
+/**
+ * @author Pascal Christoph (dr0i)
+ */
+public final class URLEncode extends AbstractSimpleStatelessFunction {
+	@Override
+	public String process(final String value) {
+		return UrlEscapers.urlFragmentEscaper().escape(value);
+	}
+}
