5c5b4d94ccff30538ffe7d7eb4184f397160b17f
pascal Christoph <christoph@hbz-nrw.de>
2014-07-23 17:28:44 +0200

    Add test processing srcipt

---
+++ lodmill-ld/doc/scripts/processTestHbz01.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: `basename $0` ES-SERVER ES-CLUSTER"
+  exit 65
+fi
+
+ES_SERVER=$1
+ES_CLUSTER=$2
+
+TIME=`date '+%Y%m%d-%H%M%S'`
+
+RESOURCES=output/json-ld-lobid-resources-test
+sh convert.sh hbzlod/lobid-resourcestest/,extlod/gnd/,extlod/dewey.nt,enrich/ $RESOURCES http://lobid.org/resource lobid-resources-$TIME json-ld-lobid
+sh index.sh $RESOURCES $ES_SERVER $ES_CLUSTER NOALIAS # no alias, index not ready yet, needs items from below
+
+ITEMS=output/json-ld-lobid-items-test
+sh convert.sh hbzlod/lobid-resourcestest/ $ITEMS http://lobid.org/item lobid-resources-$TIME json-ld-lobid-item
+sh index.sh $ITEMS $ES_SERVER $ES_CLUSTER "-staging"
