dc5f3b2ef5cb77e20da7b1ca6db03b5227802b0b
pascal Christoph <christoph@hbz-nrw.de>
2014-07-28 10:43:15 +0200

    Add parameter for facultative index alias suffix

--- lodmill-ld/doc/scripts/process.sh
+++ lodmill-ld/doc/scripts/process.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# parameter 3 defines the index alias. If not given resp. just as ""
+# no suffix is appended. Usage example: "-staging".
 
 if [ $# -ne 2 ]
 then
@@ -8,6 +10,7 @@ fi
 
 ES_SERVER=$1
 ES_CLUSTER=$2
+ALIAS=$3
 
 TIME=`date '+%Y%m%d-%H%M%S'`
 
@@ -17,23 +20,23 @@ sh index.sh $NWBIB $ES_SERVER $ES_CLUSTER NOALIAS
 
 NWBIB_SPATIAL=output/json-ld-nwbib-spatial
 sh convert.sh hbzlod/nwbib/nwbib-spatial.nt $NWBIB_SPATIAL http://purl.org/lobid/nwbib-spatial nwbib-$TIME json-ld-nwbib-spatial
-sh index.sh $NWBIB_SPATIAL $ES_SERVER $ES_CLUSTER ""
+sh index.sh $NWBIB_SPATIAL $ES_SERVER $ES_CLUSTER $ALIAS
 
 ORGANISATIONS=output/json-ld-lobid-organisations
 sh convert.sh hbzlod/lobid-organisations/ $ORGANISATIONS http://lobid.org/organisation lobid-organisations-$TIME json-ld-lobid-orgs
-sh index.sh $ORGANISATIONS $ES_SERVER $ES_CLUSTER ""
+sh index.sh $ORGANISATIONS $ES_SERVER $ES_CLUSTER $ALIAS
 
 LOBID_TEAM=output/lobid-team
 sh convert.sh hbzlod/lobid-team $LOBID_TEAM http://lobid.org/team lobid-team-$TIME json-ld-lobid-team
-sh index.sh $LOBID_TEAM $ES_SERVER $ES_CLUSTER ""
+sh index.sh $LOBID_TEAM $ES_SERVER $ES_CLUSTER $ALIAS
 
 LOBID_COLLECTIONS=output/lobid-collections
 sh convert.sh hbzlod/lobid-collections $LOBID_COLLECTIONS http://lobid.org/resource lobid-collections-$TIME json-ld-lobid-collection
-sh index.sh $LOBID_COLLECTIONS $ES_SERVER $ES_CLUSTER ""
+sh index.sh $LOBID_COLLECTIONS $ES_SERVER $ES_CLUSTER $ALIAS
 
 GND=output/json-ld-gnd
 sh convert.sh extlod/gnd/ $GND http://d-nb.info/gnd gnd-$TIME json-ld-gnd
-sh index.sh $GND $ES_SERVER $ES_CLUSTER ""
+sh index.sh $GND $ES_SERVER $ES_CLUSTER $ALIAS
 
 RESOURCES=output/json-ld-lobid-resources
 sh convert.sh hbzlod/lobid-resources/,extlod/gnd/,extlod/dewey.nt,enrich/ $RESOURCES http://lobid.org/resource lobid-resources-$TIME json-ld-lobid
@@ -41,4 +44,4 @@ sh index.sh $RESOURCES $ES_SERVER $ES_CLUSTER NOALIAS # no alias, index not read
 
 ITEMS=output/json-ld-lobid-items
 sh convert.sh hbzlod/lobid-resources/ $ITEMS http://lobid.org/item lobid-resources-$TIME json-ld-lobid-item
-sh index.sh $ITEMS $ES_SERVER $ES_CLUSTER ""
+sh index.sh $ITEMS $ES_SERVER $ES_CLUSTER $ALIAS
