caaca23f36ee82196bcf501c90f3f1e2d38939d4
pascal Christoph <christoph@hbz-nrw.de>
2014-08-05 10:51:58 +0200

    Fix broken optimizing command
    See #331
    
    * add time to log output

--- lodmill-ld/doc/scripts/index.sh
+++ lodmill-ld/doc/scripts/index.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+PRINT_TIME () {
+ printf "$(date '+%y/%m/%d %H:%M:%S')"
+}
 if [ $# -ne 5 ]
 then
   printf "Usage: `basename $0` INPUT ES_SERVER ES_CLUSTER INDEX_ALIAS INDEX_NAME\n"
@@ -15,10 +18,10 @@ INDEX_ALIAS=$4
 INDEX_NAME=$5
 
 ES_URL="http://$ES_SERVER:9200/$INDEX_NAME/"
-printf "create index in $ES_URL\n"
+PRINT_TIME ;printf "Create index in $ES_URL\n"
 curl -XPUT "$ES_URL"
 
-printf "stop index refreshing\n" 
+PRINT_TIME ; printf "Start stopping index refreshing ... \n" 
 curl -XPUT "$ES_URL"/_settings -d '{
     "index" : {
         "refresh_interval" : "-1"
@@ -26,10 +29,11 @@ curl -XPUT "$ES_URL"/_settings -d '{
 
 $HADOOP/bin/hadoop jar ../../target/lodmill-ld-*-with-dependencies.jar org.lobid.lodmill.hadoop.IndexFromHdfsInElasticSearch hdfs://10.9.0.10:8020/ $INPUT $ES_SERVER $ES_CLUSTER $INDEX_ALIAS
 
-printf "start index refreshing\n"
+PRINT_TIME ;printf "Start index refreshing...\n"
 curl -XPUT "$ES_URL"/_settings -d '{
     "index" : {
         "refresh_interval" : "1s"
     } }'
-printf "optimize the index\n"
-$ curl -XPOST "$ES_URL"/_optimize
+PRINT_TIME ;printf "Done index refreshing. Optimizing the index ...\n"
+curl -XPOST "$ES_URL"/_optimize
+PRINT_TIME ;printf "Done index optimizing.\n"
