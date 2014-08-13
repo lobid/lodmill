d30563b7e5e485eed8de7fceb755109f4107f063
pascal Christoph <christoph@hbz-nrw.de>
2014-08-13 15:55:43 +0200

    Add index parameter to test script

--- lodmill-ld/doc/scripts/processTestHbz01.sh
+++ lodmill-ld/doc/scripts/processTestHbz01.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# parameter 3 defines the branch to be used.
-# Index alias is automatically set to "-staging".
+# parameter 3 defines the branch.
+# Index alias is hardcoded to "-staging".
 
 if [ $# -lt 3 ]
 then
@@ -21,10 +21,12 @@ cd  "$THIS"
 
 TIME=`date '+%Y%m%d-%H%M%S'`
 
+INDEX_NAME=lobid-resources-$TIME
+
 RESOURCES=output/json-ld-lobid-resources-test
-sh convert.sh hbzlod/lobid-resourcestest/ $RESOURCES http://lobid.org/resource lobid-resources-$TIME json-ld-lobid
-sh index.sh $RESOURCES $ES_SERVER $ES_CLUSTER NOALIAS # no alias, index not ready yet, needs items from below
+sh convert.sh hbzlod/lobid-resourcestest/ $RESOURCES http://lobid.org/resource $INDEX_NAME json-ld-lobid
+sh index.sh $RESOURCES $ES_SERVER $ES_CLUSTER NOALIAS $INDEX_NAME # no alias, index not ready yet, needs items from below
 
 ITEMS=output/json-ld-lobid-items-test
-sh convert.sh hbzlod/lobid-resourcestest/ $ITEMS http://lobid.org/item lobid-resources-$TIME json-ld-lobid-item
-sh index.sh $ITEMS $ES_SERVER $ES_CLUSTER "-staging"
+sh convert.sh hbzlod/lobid-resourcestest/ $ITEMS http://lobid.org/item $INDEX_NAME json-ld-lobid-item
+sh index.sh $ITEMS $ES_SERVER $ES_CLUSTER "-staging" $INDEX_NAME
