dda243c1e0e087ea872d8d370d3694a16221f694
pascal Christoph <christoph@hbz-nrw.de>
2014-08-18 16:52:43 +0200

    Add filter to English and German prefLabel
    See #536
    
    * write to hdfs

--- lodmill-ld/doc/scripts/preprocessDewey.sh
+++ lodmill-ld/doc/scripts/preprocessDewey.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# filter to only get skos:prefLabel. Rewrite URI.
 
-grep  'skos/core#prefLabel' /files/open_data/closed/dewey/dewey.nt  |sed -e 's#20../../about\...##g' | sort -u >  dewey_preprocessed.nt
+# - filter to only get Englisch and German skos:prefLabel
+# - Rewrite URI
+# - remove old file from hdfs and write new one
+
+
+HDFS_FILE="extlod/dewey_preprocessed.nt"
+hadoop fs -rm $HDFS_FILE
+grep 'skos/core#prefLabel' /files/open_data/closed/dewey/dewey.nt  | egrep "(@de|@en)" | sed -e 's#20../../about\...##g' | sort -u | hadoop dfs -put - $HDFS_FILE
