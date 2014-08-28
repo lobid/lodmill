dd8f7d3079d79743de74c26ef2c297a5cd8f80aa
pascal Christoph <christoph@hbz-nrw.de>
2014-08-28 13:42:17 +0200

    Add gnd test set

--- lodmill-rd/doc/scripts/hbz01/createTestSet.sh
+++ lodmill-rd/doc/scripts/hbz01/createTestSet.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-SOURCE_PATH=/files/open_data/closed/hbzvk/snapshot
+
+SOURCE_ROOT="/files/open_data/closed/"
+SOURCE_PATH=$SOURCE_ROOT/hbzvk/snapshot
 TARGET=/files/open_data/closed/hbzvk/test/
 TMP_FILE=testIdsTmp.txt
 
 # include all junit test records
+echo "building hbz01 test set ..."
 tar fjt  ../../../src/test/resources/hbz01XmlClobs.tar.bz2 | cut -d '/' -f6 > $TMP_FILE
 cat testIds.txt >> $TMP_FILE
 for i in $(cat $TMP_FILE); do
@@ -11,3 +14,18 @@ for i in $(cat $TMP_FILE); do
 	cp $SOURCE_PATH/$SOURCE_SUBPATH/$i.bz2 $TARGET
 done
 
+#gnd 
+echo "building gnd test set ..."
+ITEM_FILE_NT="gndTest.nt"
+SOURCE_PATH_ITEM="$SOURCE_ROOT/gnd/gnd_snapshot"
+HDFS_FILE="extlod/gnd-test/$ITEM_FILE_NT"
+TARGET_ITEM="/files/open_data/closed/gnd/gnd_snapshot/test/$ITEM_FILE_NT"
+
+hadoop fs -rm $HDFS_FILE
+
+for i in $(sed -n 's#.*d-nb\.info/gnd/\(.*\)>.*#\1#p'  ../../../src/test/resources/hbz01.nt);do
+	a=$(echo $i | sed 's#\(...\).*#\1#g')
+	cat $SOURCE_PATH_ITEM/$a/$i.nt >> $TARGET_ITEM 
+done
+
+cat $TARGET_ITEM |  hadoop dfs -put - $HDFS_FILE
