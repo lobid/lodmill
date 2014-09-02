25d93ea2360e0474d1ddeff3ac63dc6eacff778a
pascal Christoph <christoph@hbz-nrw.de>
2014-09-02 13:31:39 +0200

    WIP

--- lodmill-ld/doc/scripts/processTest.sh
+++ lodmill-ld/doc/scripts/processTest.sh
@@ -25,14 +25,11 @@ TIME=`date '+%Y%m%d-%H%M%S'`
 
 GND=output/json-ld-gnd
 INDEX_NAME=gnd-$TIME
-sh convert.sh extlod/gnd-test/ $GND http://d-nb.info/gnd $INDEX_NAME json-ld-gnd
-sh index.sh $GND $ES_SERVER $ES_CLUSTER "-staging" $INDEX_NAME
+sh convert.sh extlod/gnd-test/ $GND http://d-nb.info/gnd $INDEX_NAME json-ld-gnd "-smallTest"
 
 INDEX_NAME=lobid-resources-$TIME
 RESOURCES=output/json-ld-lobid-resources-test
-sh convert.sh hbzlod/lobid-resourcestest/,extlod/dewey_preprocessed.nt,extlod/gnd-test/gndTest.nt $RESOURCES http://lobid.org/resource $INDEX_NAME json-ld-lobid
-sh index.sh $RESOURCES $ES_SERVER $ES_CLUSTER NOALIAS $INDEX_NAME # no alias, index not ready yet, needs items from below
+sh convert.sh hbzlod/lobid-resourcestest/,extlod/dewey_preprocessed.nt,extlod/gnd-test/gndTest.nt $RESOURCES http://lobid.org/resource $INDEX_NAME json-ld-lobid NOALIAS # no alias, index not ready yet, needs items from below
 
 ITEMS=output/json-ld-lobid-items-test
-sh convert.sh hbzlod/lobid-resourcestest/ $ITEMS http://lobid.org/item $INDEX_NAME json-ld-lobid-item
-sh index.sh $ITEMS $ES_SERVER $ES_CLUSTER "-staging" $INDEX_NAME
+sh convert.sh hbzlod/lobid-resourcestest/ $ITEMS http://lobid.org/item $INDEX_NAME json-ld-lobid-item "-smallTest"
