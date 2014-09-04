c214526ed1b701937e152a6fe5afd65e9a363885
Fabian Steeg <fsteeg@gmail.com>
2014-09-04 11:11:32 +0200

    Add missing parameters in convert.sh

--- lodmill-ld/doc/scripts/convert.sh
+++ lodmill-ld/doc/scripts/convert.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $# -ne 6 ]
+if [ $# -ne 8 ]
 then
-  echo "Usage: `basename $0` INPUT OUTPUT SUBJECT INDEX TYPE INDEX_ALIAS_SUFFIX"
+  echo "Usage: `basename $0` INPUT OUTPUT SUBJECT INDEX TYPE INDEX_ALIAS_SUFFIX ES_SERVER ES_CLUSTER_NAME"
   exit 65
 fi
 
@@ -17,9 +17,11 @@ SUBJ=$3
 INDEX=$4
 TYPE=$5
 INDEX_ALIAS=$6
+ES_SERVER=$7
+ES_CLUSTER_NAME=$8
 
 $HADOOP/bin/hadoop fs -rmr $TMP
 $HADOOP/bin/hadoop fs -rmr $OUT
 $HADOOP/bin/hadoop fs -rmr *.map
 $HADOOP/bin/hadoop org.lobid.lodmill.hadoop.CollectSubjects $IN $TMP $SUBJ $INDEX
-$HADOOP/bin/hadoop org.lobid.lodmill.hadoop.NTriplesToJsonLd $IN $TMP $OUT $INDEX $TYPE $SUBJ $INDEX_ALIAS
+$HADOOP/bin/hadoop org.lobid.lodmill.hadoop.NTriplesToJsonLd $IN $TMP $OUT $INDEX $TYPE $SUBJ $INDEX_ALIAS $ES_SERVER $ES_CLUSTER_NAME
