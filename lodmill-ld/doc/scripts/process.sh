561888bca5f8aee4632422f37a2bc17441e04490
pascal Christoph <christoph@hbz-nrw.de>
2014-09-18 17:46:10 +0200

    Add separate hdfs directory for fulldump database

--- lodmill-ld/doc/scripts/process.sh
+++ lodmill-ld/doc/scripts/process.sh
@@ -36,8 +36,8 @@ sh convert.sh extlod/gnd/ $GND http://d-nb.info/gnd $INDEX_NAME json-ld-gnd "$AL
 
 RESOURCES=output/json-ld-lobid-resources
 INDEX_NAME=lobid-resources-$TIME
-sh convert.sh hbzlod/lobid-resources/,extlod/gnd/,extlod/dewey_preprocessed.nt,enrich/ $RESOURCES http://lobid.org/resource $INDEX_NAME json-ld-lobid NOALIAS COLLECT
+sh convert.sh hbzlod/lobid-resources-All/,extlod/gnd/,extlod/dewey_preprocessed.nt,enrich/ $RESOURCES http://lobid.org/resource $INDEX_NAME json-ld-lobid NOALIAS COLLECT
 
 ITEMS=output/json-ld-lobid-items
 INDEX_NAME=lobid-resources-$TIME
-sh convert.sh hbzlod/lobid-resources/ $ITEMS http://lobid.org/item $INDEX_NAME json-ld-lobid-item "$ALIAS"
+sh convert.sh hbzlod/lobid-resources-All/ $ITEMS http://lobid.org/item $INDEX_NAME json-ld-lobid-item "$ALIAS"
