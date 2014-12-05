48813fe3f307b7815355fe72049b5faff81fff17
pascal Christoph <christoph@hbz-nrw.de>
2014-12-05 10:12:14 +0100

    Make "install-dependencies.sh" reusable
    * move a "sudo" command to travis installation script
    * add some "git pull" in the case the repo couldn't be cloned as it exists already
    
    This makes "install-dependencies.sh" reusable by other building
    processes , e.g. when deploying to a server.

--- lodmill-rd/install-dependencies.sh
+++ lodmill-rd/install-dependencies.sh
@@ -2,10 +2,10 @@
 cd ../..
 git clone git://github.com/culturegraph/metafacture-core.git metafacture-core-dependency
 cd metafacture-core-dependency
+git pull
 mvn clean install -DskipTests=true
 cd ..
 git clone https://github.com/hbz/metafacture-core.git
 cd metafacture-core
+git pull
 mvn clean install -DskipTests=true
-sudo apt-get install libaio-dev
-
