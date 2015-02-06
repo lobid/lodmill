25358724a6431599e740247ba5176f32e6bcf5e4
dr0i <christoph@hbz-nrw.de>
2015-02-06 11:32:32 +0100

    Add switch to do updates without enriching
    See #645.

--- lodmill-ld/doc/scripts/processResources.sh
+++ lodmill-ld/doc/scripts/processResources.sh
@@ -14,13 +14,17 @@ TIME=`date '+%Y%m%d-%H%M%S'`
 
 INDEX_NAME=lobid-resources-$TIME
 
+if [ $SET != "Updates" ]; then
+	ENRICH=",enrich/"
+fi
+
 HDFS_FILE="hbzlod/lobid-resources-$SET/resources-dump.nt"
 echo "Dumping to $HDFS_FILE ..."
 hadoop fs -rm $HDFS_FILE
 time bash -x mysqlDumpTable.sh $SET | hadoop dfs -put - $HDFS_FILE
 echo "done dumping!"
 
-time sh convert.sh hbzlod/lobid-resources-$SET/,extlod/gnd/,extlod/dewey_preprocessed.nt,enrich/ http://lobid.org/resource $INDEX_NAME json-ld-lobid NOALIAS COLLECT
+time sh convert.sh hbzlod/lobid-resources-$SET/,extlod/gnd/,extlod/dewey_preprocessed.nt$ENRICH http://lobid.org/resource $INDEX_NAME json-ld-lobid NOALIAS COLLECT
 echo "done resources!"
 
 time sh convert.sh hbzlod/lobid-resources-$SET/ http://lobid.org/item $INDEX_NAME json-ld-lobid-item "$ALIAS"
