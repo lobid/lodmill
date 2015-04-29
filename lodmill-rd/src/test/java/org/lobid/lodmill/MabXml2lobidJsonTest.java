501e72f822e3259cac6bcb37d46fde2e1deb18b0
dr0i <christoph@hbz-nrw.de>
2015-04-29 18:35:28 +0200

    Add RdfModel2ElasticsearchJsonLd
    See hbz/lobid#139.
    
    * add test and test data
    
    This metafacture module generates json-ld from a jena rdf model.
    The generated documents are ready to be elasticsearch bulk indexed.
    
    There are highly specific requirements for generating documents out of the
    hbz01 catalog graph build with morph. One hbz01 catalog entry may result in
    dozens of documents, namely the
    
    - 'main' doc (data about the main resource)
    - 'items' of a doc
    - 'super' docs ("hasPart" nodes)
    - 'sameAs' docs
    
    But not all nodes should be nodes on their own: the gnd nodes must stay
    sub nodes of the main node.
    
    So this module is not generic and may be made generic only to a certain degree.

---
+++ lodmill-rd/src/test/java/org/lobid/lodmill/MabXml2lobidJsonTest.java
@@ -0,0 +1,57 @@
+/* Copyright 2015  hbz, Pascal Christoph.
+ * Licensed under the Eclipse Public License 1.0 */
+package org.lobid.lodmill;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.culturegraph.mf.morph.Metamorph;
+import org.culturegraph.mf.stream.converter.xml.XmlDecoder;
+import org.culturegraph.mf.stream.sink.ObjectFileWriter;
+import org.culturegraph.mf.stream.source.FileOpener;
+import org.junit.Test;
+
+/**
+ * Transform hbz01 Aleph Mab XML catalog data into lobid elasticsearch ready
+ * JSON-LD.
+ * 
+ * @author Pascal Christoph (dr0i)
+ * 
+ */
+@SuppressWarnings("javadoc")
+public final class MabXml2lobidJsonTest {
+
+	private static final String TEST_FILENAME = "hbz01.json";
+
+	@SuppressWarnings("static-method")
+	@Test
+	public void testFlow() throws URISyntaxException {
+		buildAndExecuteFlow();
+		File testFile = new File(TEST_FILENAME);
+		AbstractIngestTests.compareFilesDefaultingBNodes(testFile, new File(Thread
+				.currentThread().getContextClassLoader().getResource(TEST_FILENAME)
+				.toURI()));
+		testFile.deleteOnExit();
+	}
+
+	private static void buildAndExecuteFlow() {
+		// hbz catalog transformation
+		final FileOpener opener = new FileOpener();
+		opener.setCompression("BZIP2");
+		final Triples2RdfModel triple2model = new Triples2RdfModel();
+		triple2model.setInput("N-TRIPLE");
+		opener
+				.setReceiver(new TarReader())
+				.setReceiver(new XmlDecoder())
+				.setReceiver(new MabXmlHandler())
+				.setReceiver(
+						new Metamorph("src/main/resources/morph-hbz01-to-lobid.xml"))
+				.setReceiver(new PipeEncodeTriples()).setReceiver(triple2model)
+				.setReceiver(new RdfModel2ElasticsearchJsonLd())
+				.setReceiver(new ObjectFileWriter<String>(TEST_FILENAME));
+		opener.process(new File("src/test/resources/hbz01XmlClobs.tar.bz2")
+				.getAbsolutePath());
+		opener.closeStream();
+
+	}
+}
