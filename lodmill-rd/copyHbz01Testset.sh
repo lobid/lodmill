b07e379e7a50fe45ac36e8488ef919abd9c3c75c
dr0i <christoph@hbz-nrw.de>
2015-05-18 15:14:31 +0200

    Add handling GND entities with complex names
    See hbz/lobid#145.
    
    * update morph xml (renaming "@-variable", adjust logic)
    * adjust metamorph.xsd to allow setSameEntity using "concat"
    * add test resource HT004381366 and HT014215912
    * update tests
    * update diff script to use unique sort

--- lodmill-rd/copyHbz01Testset.sh
+++ lodmill-rd/copyHbz01Testset.sh
@@ -4,7 +4,7 @@
 # Paramter 1: use "nt" or "json"
 
 FILE_TYPE="$1"
-sort hbz01.$FILE_TYPE |sort -g > hbz01.$FILE_TYPE.sorted ; sort -g src/test/resources/hbz01.$FILE_TYPE >b ; diff b hbz01.$FILE_TYPE.sorted ; echo "^<=old vs. ^>new" ; echo  "cp hbz01.$FILE_TYPE.sorted src/test/resources/hbz01.$FILE_TYPE"
+sort -g hbz01.$FILE_TYPE | sort -u > hbz01.$FILE_TYPE.sorted ; sort -g src/test/resources/hbz01.$FILE_TYPE >b ; diff b hbz01.$FILE_TYPE.sorted ; echo "^<=old vs. ^>new" ; echo  "cp hbz01.$FILE_TYPE.sorted src/test/resources/hbz01.$FILE_TYPE"
 
 IFS="
 ";
