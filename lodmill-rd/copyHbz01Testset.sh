21f2926cfcdec912873fcb0624143b7eee41ad6b
dr0i <christoph@hbz-nrw.de>
2015-12-20 20:13:28 +0100

    Add regex to diff ignoring bnodes
    - add diff of CT003012479.nt for uri urn test

--- lodmill-rd/copyHbz01Testset.sh
+++ lodmill-rd/copyHbz01Testset.sh
@@ -2,7 +2,9 @@
 # Check difference between test data and newly created test data. Convenient when
 # updating mapping. If diff is ok, you copy the new test data to the test data.
 # Paramter 1: use "nt" or "es.nt"
-
+echo "CT003012479.nt:"
+diff tmp/nt/00301/CT003012479.nt  src/test/resources/CT003012479.nt | sed -e 's#[<>] _:[[:alnum:]]*\ .*##g' | sed -e 's#^[[:digit:]].*##g' | sort -u
+echo "----------------------------------------------\n"
 FILE_TYPE="$1"
 sort -g hbz01.$FILE_TYPE |
 sort -u > hbz01.$FILE_TYPE.sorted
@@ -10,10 +12,12 @@ sort -u > hbz01.$FILE_TYPE.sorted
 sort -g src/test/resources/hbz01.$FILE_TYPE |
 sort -u >b
 
-diff b hbz01.$FILE_TYPE.sorted
+diff b hbz01.$FILE_TYPE.sorted | sed -e 's#[<>] _:[[:alnum:]]*\ .*##g' | sed -e 's#^[[:digit:]].*##g' | sort -u
+
 rm b
 echo "^<=old vs. ^>new"
 echo  "cp hbz01.$FILE_TYPE.sorted src/test/resources/hbz01.$FILE_TYPE"
 
 IFS="
 ";
+
