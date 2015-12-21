769ee2215de49e6123375a11a157ca603017c3dc
dr0i <christoph@hbz-nrw.de>
2015-12-21 15:52:38 +0100

    Refactor to make diffs even nicer
    - handle bnodes properly

--- lodmill-rd/copyHbz01Testset.sh
+++ lodmill-rd/copyHbz01Testset.sh
@@ -2,22 +2,22 @@
 # Check difference between test data and newly created test data. Convenient when
 # updating mapping. If diff is ok, you copy the new test data to the test data.
 # Paramter 1: use "nt" or "es.nt"
-echo "CT003012479.nt:"
-diff tmp/nt/00301/CT003012479.nt  src/test/resources/CT003012479.nt | sed -e 's#[<>] _:[[:alnum:]]*\ .*##g' | sed -e 's#^[[:digit:]].*##g' | sort -u
-echo "----------------------------------------------\n"
-FILE_TYPE="$1"
-sort -g hbz01.$FILE_TYPE |
-sort -u > hbz01.$FILE_TYPE.sorted
-
-sort -g src/test/resources/hbz01.$FILE_TYPE |
-sort -u >b
-
-diff b hbz01.$FILE_TYPE.sorted | sed -e 's#[<>] _:[[:alnum:]]*\ .*##g' | sed -e 's#^[[:digit:]].*##g' | sort -u
-
-rm b
 echo "^<=old vs. ^>new"
-echo  "cp hbz01.$FILE_TYPE.sorted src/test/resources/hbz01.$FILE_TYPE"
 
-IFS="
-";
+case $1 in
+	CT) echo "Nice diff on CT003012479.nt"
+	diff src/test/resources/CT003012479.nt tmp/nt/00301/CT003012479.nt | sed -e 's# _:[[:alnum:]]*\(.*\)#\1#g' |  sed -e 's#\(.*\)_:[[:alnum:]]*#\1#g' |sed -e 's#^[[:digit:]].*##g' | sort | uniq -u -f2 ;;
+	*)
+	FILE_TYPE="$1"
+	sort -g hbz01.$FILE_TYPE |
+	sort -u > hbz01.$FILE_TYPE.sorted
 
+	sort -g src/test/resources/hbz01.$FILE_TYPE |
+	sort -u >b
+
+	diff b hbz01.$FILE_TYPE.sorted | sed -e 's# _:[[:alnum:]]*\(.*\)#\1#g' |  sed -e 's#\(.*\)_:[[:alnum:]]*#\1#g' |sed -e 's#^[[:digit:]].*##g' | sort | uniq -u -f2
+
+	rm b
+	echo "^<=old vs. ^>new"
+	echo  "cp hbz01.$FILE_TYPE.sorted src/test/resources/hbz01.$FILE_TYPE"
+esac
